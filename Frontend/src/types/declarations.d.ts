e549db882f08663d3c9a221691d8598a150c6e98
andyhu <andywho@student.ubc.ca>
2024-07-18 13:15:15 -0700

    trying to solve fe build issue

--- Frontend/src/types/declarations.d.ts
+++ Frontend/src/types/declarations.d.ts
@@ -1,3 +1,18 @@
+// // src/types/declarations.d.ts
+// declare module './App.jsx';
+// declare module './redux/store.js';
+
 // src/types/declarations.d.ts
-declare module './App.jsx';
-declare module './redux/store.js';
+
+declare module './App.jsx' {
+    import { FC } from 'react';
+    const App: FC;
+    export default App;
+  }
+  
+  declare module './redux/store.js' {
+    import { Store } from 'redux';
+    const store: Store;
+    export default store;
+  }
+  
