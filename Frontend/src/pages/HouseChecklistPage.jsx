d898857150cc8e13272a6123f340fe7aae0c34e1
Justin Burden <jburden1@students.ubc.ca>
2024-07-24 08:23:36 -0700

    initial setup of HouseChecklistPage and UnitExteriorChecklistTable

--- Frontend/src/pages/HouseChecklistPage.jsx
+++ Frontend/src/pages/HouseChecklistPage.jsx
@@ -2,22 +2,25 @@ import React from 'react'
 import { Box, Container, Typography } from '@mui/material'
 import Navbar from '../components/navigation/Navbar'
 import HouseTabs from '../components/navigation/HouseTabs'
-import Header1 from '../components/headers/Header1.jsx'
-import HouseHeader from '../components/headers/HouseHeader.jsx'
+import HouseHeader from '../components/headers/HouseHeader'
+import Header1 from '../components/headers/Header1'
+import UnitExteriorChecklistTable from '../components/tables/checklists/UnitExteriorChecklistTable'
 import { useSelector } from 'react-redux'
 
-const HouseDocumentsPage = () => {
+const HouseChecklistPage = () => {
   const houseInfo = useSelector(state => state.houses.findHouse)
 
   return (
     <Navbar>
       <Container>
         <Header1 title={<HouseHeader npl={houseInfo?.npl} />} />
-
         <HouseTabs />
+        <Box mt={3}>
+          <UnitExteriorChecklistTable />
+        </Box>
       </Container>
     </Navbar>
   )
 }
 
-export default HouseDocumentsPage
+export default HouseChecklistPage
