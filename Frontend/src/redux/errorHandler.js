e15110ca7badf23ad5b3f64862b69fdda55b655b
andyhu <andywho@student.ubc.ca>
2024-07-29 15:55:27 -0700

    updated error handler message

--- Frontend/src/redux/errorHandler.js
+++ Frontend/src/redux/errorHandler.js
@@ -2,7 +2,7 @@ export const errorHandler = (error, callerName) => {
     if (error.message === 'Network Error' && error.name === 'AxiosError') {
         alert(`There's a network error fetching data from backend when trying to ${callerName}`);
     } else {
-        console.error(`Error in ${callerName}:`, error);
+        console.error(`Error handler found error in caller ${callerName}:`, error);
     }
 }
 
