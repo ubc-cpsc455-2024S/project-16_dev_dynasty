764fff9a2c382367dc60806d1ec8b6b3a067c3a6
Justin Burden <jburden1@student.ubc.ca>
2024-07-30 22:07:04 -0700

    changed Customer Name selction component on HousesAddPage to autocomplete component

--- Frontend/src/pages/HousesAddPage.jsx
+++ Frontend/src/pages/HousesAddPage.jsx
@@ -1,13 +1,26 @@
 import React, { useEffect } from 'react'
 import Navbar from '../components/navigation/Navbar'
 import Header1 from '../components/headers/Header1'
-import { TextField, Button, Box, Typography, MenuItem } from '@mui/material'
+import {
+  TextField,
+  Button,
+  Box,
+  Typography,
+  MenuItem,
+  Autocomplete,
+} from '@mui/material'
 import { useDispatch, useSelector } from 'react-redux'
 import { useState } from 'react'
 import { addHouseAsync } from '../redux/houses/thunksHouses.js'
 import { getCustomersAsync } from '../redux/customers/thunksCustomers.js'
 import { useNavigate } from 'react-router-dom'
 import { putChecklistAsync } from '../redux/checklists/thunksChecklists.js'
+import { createFilterOptions } from '@mui/material/Autocomplete'
+
+const customerFilterOptions = createFilterOptions({
+  matchFrom: 'start',
+  stringify: option => option.customer_name,
+})
 
 const HousesAddPage = () => {
   const dispatch = useDispatch()
@@ -20,6 +33,8 @@ const HousesAddPage = () => {
     square_ft: '',
   }
   const [formFields, setFormFields] = useState(initialState)
+  const [customerValue, setCustomerValue] = useState(null)
+  const [customerInputValue, setCustomerInputValue] = useState('')
 
   const customers = useSelector(state => state.customers.list)
 
@@ -55,20 +70,23 @@ const HousesAddPage = () => {
               value={formFields.npl}
               onChange={handleChange}
             />
-            <TextField
-              required
-              select={true}
-              name={'customer_id'}
-              label={'Customer Name'}
-              value={formFields.customer_id}
-              onChange={handleChange}
-            >
-              {customers.map(customer => (
-                <MenuItem key={customer._id} value={customer._id}>
-                  {customer.customer_name}
-                </MenuItem>
-              ))}
-            </TextField>
+            <Autocomplete
+              value={customerValue}
+              onChange={(event, newValue) => {
+                setCustomerValue(newValue)
+                setFormFields({ ...formFields, customer_id: newValue._id })
+              }}
+              inputValue={customerInputValue}
+              onInputChange={(event, newInputValue) => {
+                setCustomerInputValue(newInputValue)
+              }}
+              options={customers}
+              filterOptions={customerFilterOptions}
+              getOptionLabel={option => option.customer_name}
+              renderInput={params => (
+                <TextField {...params} label='Customer Name' required />
+              )}
+            />
             <TextField
               required
               name={'house_model'}
