75a039c59b26afacfb6b4c7fa38a3f65bdb8dc57
andyhu <andywho@student.ubc.ca>
2024-08-02 12:58:18 -0700

    using box instead of filtercontainer

--- Frontend/src/pages/EventLogPage.jsx
+++ Frontend/src/pages/EventLogPage.jsx
@@ -1,119 +1,4 @@
-// import React, { useEffect } from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-// import Navbar from '../components/navigation/Navbar'
-// import Header1 from '../components/headers/Header1'
-// import { Box, CircularProgress, Container, Typography } from '@mui/material'
-// import { getEventLogsAsync } from '../redux/logs/thunkLog'
-// import { styled } from '@mui/system'
-// import { colors } from '../styles/colors'
-
-// const EventLogPage = () => {
-//   const eventLogs = useSelector(state => state.logs.eventLogs)
-//   const status = useSelector(state => state.logs.status)
-//   const error = useSelector(state => state.logs.error)
-//   const dispatch = useDispatch()
-
-//   useEffect(() => {
-//     dispatch(getEventLogsAsync())
-//   }, [dispatch])
-
-//   const ErrorText = styled(Typography)({
-//     color: colors.errorTextColor,
-//     textAlign: 'center',
-//   })
-//   const LoadingContainer = styled(Box)({
-//     display: 'flex',
-//     justifyContent: 'center',
-//   })
-
-//   const TimeTypography = styled(Typography)({
-//     flex: '0 0 15%', // 15% width
-//   })
-
-//   const TypeTypography = styled(Typography)({
-//     flex: '0 0 15%', // 15% width
-//   })
-
-//   const ContentTypography = styled(Typography)({
-//     flex: '0 0 70%', // 70% width
-//   })
-
-//   const LogContainer = styled(Box)({
-//     display: 'flex',
-//     flexDirection: 'row',
-//     justifyContent: 'space-between',
-//     alignItems: 'center',
-//     padding: '8px 16px',
-//     background: "#616161",
-//     borderBottom: '1px solid #ccc',
-//   })
-
-//   const LogsBox = styled(Box)({
-//     maxHeight: '75vh',
-//     // width: '80vw',
-//     overflow: 'auto',
-//     border: '1px solid #ccc',
-//     borderRadius: '8px',
-//     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-//     '&::-webkit-scrollbar': {
-//       width: '10px',
-//     },
-//     '&::-webkit-scrollbar-thumb': {
-//       background: '#888',
-//       borderRadius: '10px',
-//     },
-//     '&::-webkit-scrollbar-thumb:hover': {
-//       background: '#555',
-//     },
-//   })
-
-//   const HeaderContainer = styled(Box)({
-//     display: 'flex',
-//     flexDirection: 'row',
-//     justifyContent: 'space-between',
-//     alignItems: 'center',
-//     padding: '8px 16px',
-//     background: "#9e9e9e",
-//     borderBottom: '1px solid #ccc',
-//     position: 'sticky',
-//     top: 0,
-//     zIndex: 1,
-//   })
-
-//   return (
-//     <Navbar>
-//       <Container>
-//         <Header1 title={'Event Logs'} />
-//         <LogsBox mt={3}>
-//           <HeaderContainer>
-//             <TimeTypography variant="h6">Event Time</TimeTypography>
-//             <ContentTypography variant="h6">Log Content</ContentTypography>
-//             <TypeTypography variant="h6">Event Type</TypeTypography>
-//           </HeaderContainer>
-//           {status === 'pending' ? (
-//             <LoadingContainer>
-//               <CircularProgress />
-//             </LoadingContainer>
-//           ) : error ? (
-//             <ErrorText>Error: {error}</ErrorText>
-//           ) : (
-//             eventLogs.map((log, index) => (
-//               <LogContainer key={index}>
-//                 <TimeTypography variant="body1">{log.time}</TimeTypography>
-//                 <ContentTypography variant="body1">{log.content}</ContentTypography>
-//                 <TypeTypography variant="body1">{log.type}</TypeTypography>
-//               </LogContainer>
-//             ))
-//           )}
-//         </LogsBox>
-//       </Container>
-//     </Navbar>
-//   )
-// }
-
-// export default EventLogPage
-
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Navbar from '../components/navigation/Navbar'
 import Header1 from '../components/headers/Header1'
@@ -146,9 +31,10 @@ const EventLogPage = () => {
   const [npl, setNpl] = useState('')
   const [model, setModel] = useState('')
 
+
   useEffect(() => {
     dispatch(getEventLogsAsync())
-  }, [dispatch])
+  }, [])
 
   const handleYearChange = event => {
     setYear(event.target.value)
@@ -174,6 +60,7 @@ const EventLogPage = () => {
     setModel(event.target.value)
   }
 
+
   const handleResetFilters = () => {
     setYear('')
     setMonth('')
@@ -197,6 +84,8 @@ const EventLogPage = () => {
       (!model || log.logContent.toLowerCase().includes(model.toLowerCase()))
     )
   })
+
+
 
   const ErrorText = styled(Typography)({
     color: colors.errorTextColor,
@@ -262,13 +151,13 @@ const EventLogPage = () => {
     zIndex: 1,
   })
 
-  const FilterContainer = styled(Box)({
-    marginTop: '24px',
-    marginBottom: '16px',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  })
+  // const FilterContainer = styled(Box)({
+  //   marginTop: '24px',
+  //   marginBottom: '16px',
+  //   display: 'flex',
+  //   justifyContent: 'space-between',
+  //   alignItems: 'center',
+  // })
 
   const TotalLogsContainer = styled(Box)({
     textAlign: 'center',
@@ -278,7 +167,14 @@ const EventLogPage = () => {
     <Navbar>
       <Container>
         <Header1 title={'Event Logs'} />
-        <FilterContainer>
+        <Box
+        sx={{
+          marginTop: '24px',
+          marginBottom: '16px',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
           <Select value={year} onChange={handleYearChange} displayEmpty>
             <MenuItem value=''>
               <em>All Years</em>
@@ -355,14 +251,12 @@ const EventLogPage = () => {
             ))}
           </Select>
 
-
           <TextField
             id='npl'
             value={npl}
             onChange={handleNplChange}
             variant='outlined'
             label='NPL'
-           
           />
 
           <TextField
@@ -395,7 +289,7 @@ const EventLogPage = () => {
           </Select>
 
           <Button onClick={handleResetFilters}>Reset Filters</Button>
-        </FilterContainer>
+        </Box>
         <TotalLogsContainer>
           <Typography variant='h6'>
             Number of Logs: {filteredLogs.length}
