ae14a3a80f83706f515e5c90f0374d9dac5a7f59
rdick <62073529+rdick@users.noreply.github.com>
2024-08-02 14:17:36 -0600

    Modify Primary color in theme

--- Frontend/src/styles/theme.tsx
+++ Frontend/src/styles/theme.tsx
@@ -4,5 +4,8 @@ import { createTheme } from '@mui/material/styles'
 export const theme = createTheme({
   palette: {
     mode: 'dark',
+    primary: {
+      main: 'rgb(245, 145, 31)',
+    },
   },
 })
